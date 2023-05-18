315b84a48e032c714548aa4243d1a4f0c224732a
emarkees <rphillipse@gmail.com>
2023-05-18 06:36:34 -0600

    Add TodoApp.jsx files

---
+++ src/Component/TodoApp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TodosLogic from "./TodosLogic";
+
+const TodoApp =  () => {
+  return ( 
+    <div className="container">
+      <Header />
+      <TodosLogic />
+    </div>
+  );
+};
+
+export default TodoApp ;
